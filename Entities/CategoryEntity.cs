324a4b1738449ab702db0e72d55a0bf147c6bccd
JuliaGrisales <jgrisales@gmail.com>
2020-04-04 12:42:39 +0200

    Creación de entidades

---
+++ Entities/CategoryEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Entities
+{
+    public class CategoryEntity
+    {
+        [Key] // Nuestra Llave primaria
+        public string CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
