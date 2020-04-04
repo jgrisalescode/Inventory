f74e306833ee090fa85be7356a1e9fa15a994435
JuliaGrisales <jgrisales@gmail.com>
2020-04-04 12:52:39 +0200

    Data Annotations

--- Entities/CategoryEntity.cs
+++ Entities/CategoryEntity.cs
@@ -8,7 +8,11 @@ namespace Entities
     public class CategoryEntity
     {
         [Key] // Nuestra Llave primaria
+        [StringLength(50)]
         public string CategoryId { get; set; }
+
+        [Required] // Obligatorio
+        [StringLength(100)]
         public string CategoryName { get; set; }
 
     }
